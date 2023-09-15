bd973436debbc6a3b9b2f6faa0993e6c99cebd21
OneTwoDan <danielondonoa@gmail.com>
2023-09-14 21:24:29 -0500

    tech stack components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Navbar from "./components/Navbar";
-import Summary from "./components/Summary";
+/* import About from './components/About'; */
+import Summary from './components/Summary';
+import Navbar from './components/Navbar';
+/* import Projects from './components/Projects'; */
+import Skills from './components/Skills';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Summary />
+      <Skills />
+      {/* <About /> */}
+      {/* <Projects /> */}
     </>
   );
 };
-
 export default App;
